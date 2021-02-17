3c641445e088eef10bcfd775b3e18895db65ff98
Alena Gritsiuk <alena22292@mail.ru>
2021-02-17 15:40:44 +0100

    Create a new layout of About page

--- src/components/aboutItem.jsx
+++ src/components/aboutItem.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AboutItem extends Component {
   render(){
     return(
-      <li className="row flex-box">
+      <li className="row flex-box-bar">
         <span className="col-sm-12 col-lg-4">{this.props.name}</span>
         <div className="col-sm-12 col-lg-8 progressbar-container">
           <div className="progressbar-complete" style={{width: `${this.props.level}`}}>
