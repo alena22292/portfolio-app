5377b53963b389e47727a2fe8e53389b72193301
Alena Gritsiuk <alena22292@mail.ru>
2021-02-23 22:25:21 +0100

    Add a funcion of reducer to sum up all the cost in the shopping cart

--- src/components/Projects/Shop/order.jsx
+++ src/components/Projects/Shop/order.jsx
@@ -4,6 +4,7 @@ import OrderItem from './order-item';
 
 class Order extends Component {
   render() {
+    const result= this.props.order.map(item => item.price * item.quantity).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     return (
       <section className="container">
         <div style={{margin: '30px'}}className="flex-box-between">
@@ -12,12 +13,21 @@ class Order extends Component {
             <Link to="/shop"><i style={{fontSize: '40px', color: '#ef8d32'}} className="fas fa-laptop-house"></i></Link>
           </div>
         </div>
-        <div className="row">
-        {this.props.order.map(item => {
-          return <OrderItem key={item.id} {...item} functionPlus={this.props.addQ} functionMinus={this.props.desQ}/>
-         })
-        }
-        </div>
+
+          <div className="row">
+            <div className="col-sm-10">
+              <div className="row">
+              {this.props.order.map(item => {
+                return <OrderItem key={item.id} {...item} functionPlus={this.props.addQ} functionMinus={this.props.desQ}/>
+               })
+              }
+              </div>
+            </div>
+            <div className="col-sm-2 line">
+              Total: EUR {result}
+            </div>
+          </div>
+
       </section>
     );
   }
